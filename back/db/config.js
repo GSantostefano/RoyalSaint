c56bd0638a495ec17b432799f8b1f1c084625e92
GSantostefano <gabrielsantostefano@gmail.com>
2024-11-07 13:04:21 -0300

    5
    5

--- back/db/config.js
+++ back/db/config.js
@@ -1,9 +1,17 @@
-const { config } = require('./../config/config');
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgresql://${DB_NAME}_owner:${DB_PASSWORD_NEON}@${DB_HOST_CONFIG}/${DB_NAME}?sslmode=require`;
-// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+const URI = process.env.DATABASE_URL;
+
 module.exports = {
   development: { url: URI, dialect: 'postgres' },
   production: { url: URI, dialect: 'postgres' },
 };
+
+
+// const { config } = require('./../config/config');
+// const USER = encodeURIComponent(config.dbUser);
+// const PASSWORD = encodeURIComponent(config.dbPassword);
+// const URI = `postgresql://${DB_NAME}_owner:${DB_PASSWORD_NEON}@${DB_HOST_CONFIG}/${DB_NAME}?sslmode=require`;
+// module.exports = {
+//   development: { url: URI, dialect: 'postgres' },
+//   production: { url: URI, dialect: 'postgres' },
+// };
