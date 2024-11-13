4fea0022954df2649d0f454b61d53ee18c809b09
GSantostefano <gabrielsantostefano@gmail.com>
2024-11-13 14:31:39 -0300

    1
    1

--- front/src/Components/Card/index.jsx
+++ front/src/Components/Card/index.jsx
@@ -4,7 +4,7 @@
 const Card = ({ card, showProduct, renderIcon, navigate }) => {
   return (
     <div
-      className="border border-white hover:shadow-lg hover:shadow-white/50 bg-black/20 cursor-pointer w-full h-auto rounded-lg overflow-hidden transform transition-all duration-300"
+      className="border border-white/30 hover:shadow-lg hover:shadow-white/60 bg-black/40 cursor-pointer w-full h-auto rounded-lg overflow-hidden transform transition-all duration-300"
       onClick={() => showProduct(card)}
     >
       <figure className="relative w-full h-48 sm:h-52 md:h-60 lg:h-72">
@@ -16,17 +16,18 @@ const Card = ({ card, showProduct, renderIcon, navigate }) => {
             className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
             src={card.images[0]}
             alt={card.category.name}
+            
           />
         </div>
         {renderIcon(card.id)}
       </figure>
       <div className="px-4 py-3 text-center">
-        <p className="text-white text-xl font-semibold truncate">{card.title}</p>
-        <p className="text-white text-xl font-bold">${card.price}</p>
+        <p className="text-white text-2xl font-semibold truncate">{card.title}</p>
+        
       </div>
       <div className="flex justify-center pb-4">
         <button
-          className="w-10/12 md:w-4/5 lg:w-3/4 py-2 border border-black text-sm font-medium rounded-md bg-white text-black hover:bg-black hover:text-white transition-all duration-200"
+          className="w-10/12 md:w-4/5 lg:w-3/4 py-2 border border-black text-sm font-medium rounded-md bg-white text-black hover:bg-black/50 hover:text-white transition-all duration-200"
           onClick={(e) => {
             e.stopPropagation();
             navigate(`/productdetail/${card.id}`, { state: { card } });
